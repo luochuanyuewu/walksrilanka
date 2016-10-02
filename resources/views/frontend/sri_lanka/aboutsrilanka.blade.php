0faccf8a13914b043d18ee2a8f327cadf4a0e71f
luochuanyuewu <luochuanyuewu@gmail.com>
2016-10-02 19:30:55 +0800

    增加视图文件

---
+++ resources/views/frontend/sri_lanka/aboutsrilanka.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luochuanyuewu
+ * Date: 16/10/2
+ * Time: 下午7:10
+ */
--- resources/views/includes/frontend/navigation.blade.php
+++ resources/views/includes/frontend/navigation.blade.php
@@ -13,14 +13,30 @@
         <div class="collapse navbar-collapse" id="navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li id="home" class="active"><a href="/" class="animated"><span
-                                class="fa fa-home"></span> 首页</a></li>
-                <li>
-                    <a class="animated" href="#" data-toggle="modal" data-target="#developing"><span
-                                class="fa fa-gamepad"></span> 旅游套餐</a>
+                                class="fa fa-home"></span> 首页</a>
                 </li>
-                <li>
-                    <a class="animated" href="#" data-toggle="modal" data-target="#developing"><span
-                                class="fa fa-picture-o"></span> 旅游景点</a>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">斯里兰卡
+                        <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">介绍斯里兰卡</a></li>
+                        <li><a href="#">流行的目的地</a></li>
+                        <li><a href="#">有趣的项目</a></li>
+                        <li><a href="#">有趣的旅游信息</a></li>
+                        <li><a href="#">丰富的饮食</a></li>
+                    </ul>
+                </li>
+
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">旅游套餐
+                        <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Classic Mini Tour</a></li>
+                        <li><a href="#">Best of Sri Lanka</a></li>
+                        <li><a href="#">Cultural Heritage Tour</a></li>
+                        <li><a href="#">Scenic Sri Lanka</a></li>
+                        <li><a href="#">Ultimate Sri Lanka</a></li>
+                    </ul>
                 </li>
 
                 <li id="liuyan"><a href="contact" class="animated"><span
